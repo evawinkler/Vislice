11c4782d266358649f0f801c141e4a3aad92d5ff
Eva Winkler <winkler.eva00@gmail.com>
2020-05-11 18:13:00 +0200

    drugi del modela

--- model.py
+++ model.py
@@ -1,16 +1,23 @@
 
 STEVILO_DOVOLJENIH_NAPAK = 10
+#konstante za rezultate ugibanj 
+
 PRAVILNA_CRKA = '+'
 PONOVLJENA_CRKA = 'o'
 NAPACNA_CRKA = '-'
 
+#konstante za zmago in poraz
 ZMAGA = 'W'
 PORAZ = 'X'
 
+bazen_besed = []
+with open("besede.txt") as datoteka_bazena:
+    for beseda in datoteka_bazena:
+        bazen_besed.append(beseda.strip).lower()
 class Igra:
 
-    def __init__(self,geslo,crke=None):
-        self.geslo = geslo.upper()
+    def __init__(self, geslo, crke=None):
+        self.geslo = geslo
         if crke is None:
             self.crke = []
         else:
